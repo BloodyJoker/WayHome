a1599a06fa3947b5d712540335bd4478e41c52b8
bloodyjokeer <767373506@qq.om>
2018-10-07 15:47:11 +0800

    Initial commit

---
+++ Source/WayHome/WayHomeGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "WayHomeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WAYHOME_API AWayHomeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
